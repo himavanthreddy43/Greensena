7464bcee2679d9a98dec4fb236cc891f8de651cd
himavanthreddy43 <udumulahimavanthreddy@gmail.com>
2026-05-18 18:55:11 +0530

    Update frontend API URLs to Render production URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+﻿import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Users, ScanFace, LayoutDashboard } from 'lucide-react';
 import RegisterPage from './pages/RegisterPage';
 import RecognizePage from './pages/RecognizePage';
